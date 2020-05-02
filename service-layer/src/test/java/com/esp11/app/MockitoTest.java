3a1bf1a8a3ccf83582b2303c4fef7d6f2797b0d3
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2020-05-02 21:51:44 +0100

    mockito

---
+++ service-layer/src/test/java/com/esp11/app/MockitoTest.java
@@ -0,0 +1,24 @@
+package com.esp11.app;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.*;
+
+import static org.mockito.Mockito.*;
+
+public class MockitoTest {
+
+    @Mock
+    //MyDatabase databaseMock;
+    //FighterHR fighter = new FighterHR();
+
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Test
+    public void testQuery(){
+
+    }
+
+}
